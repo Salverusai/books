71a57c7ca67e54faf1c8bc3a72192d869ca8102c
500apps-saibhushanam <88704941+Salverusai@users.noreply.github.com>
2021-08-25 17:29:40 +0530

    Add files via upload

---
+++ TheForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <form>
+    <p> Books Database </p>
+    <div class="form-control">
+      <label for="user-name">Enter your Book name</label>
+      <input id="user-name" name="user-name" type="text" />
+    </div>
+    
+    
+    
+    <div class="form-control">
+      <h2>Enter the book you want to buy! </h2>
+      <input id="user-name" name="user-name" type="text" /><br>
+      
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
